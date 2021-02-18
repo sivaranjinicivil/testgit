7b01e7acf5bcb2451885849db5b81163f604116e
sivaranjinicivil <sivaranjinicivil@gmail.com>
2021-02-18 10:36:36 +0530

    colorbg

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup(){
 }
 
 function draw(){
-    background(0);
+   background("blue")
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
